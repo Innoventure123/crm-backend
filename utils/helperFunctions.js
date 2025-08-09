babe2aeb6408e1b01e2cc3fb320dd8baaf44bf86
Muhammad Siddik Akabar <siddik20@navgurukul.org>
2025-08-09 15:26:55 +0530

    changes

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -249,127 +249,146 @@ const updateAllUserPasswords = async () => {
 
 async function insertNewUser() {
 	const users = [
+		// {
+		// 	name: "Divya Lekshmi",
+		// 	email: "divya@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Bipin", email: "bipin@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Mishiel",
+		// 	email: "mishiel@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Kent", email: "kent@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Mahmoud Gaber",
+		// 	email: "mahmoud@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Sayed Atef",
+		// 	email: "sayed@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Nada", email: "nada@innoventure.ae", role: "agent", role_id: 5 },
+		// { name: "Sinan", email: "sinan@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Abdallah Salag",
+		// 	email: "salag@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Mustafa",
+		// 	email: "mustafa@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Abdallah Bakhit",
+		// 	email: "bakhit@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Amil",
+		// 	email: "amil@innoventure.ae",
+		// 	role: "team_lead",
+		// 	role_id: 4,
+		// },
+		// {
+		// 	name: "Sameer",
+		// 	email: "sameer@innoventure.ae",
+		// 	role: "team_lead",
+		// 	role_id: 4,
+		// },
+		// {
+		// 	name: "Libi",
+		// 	email: "libi@innoventure.ae",
+		// 	role: "team_lead",
+		// 	role_id: 4,
+		// },
+		// {
+		// 	name: "Chrisvel J",
+		// 	email: "chrisvel@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Imran", email: "imran@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Prabha SH",
+		// 	email: "prabha@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Talha Ansari",
+		// 	email: "talha@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Harsh", email: "harsh@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Ashwini",
+		// 	email: "ashwini@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Pratibha K",
+		// 	email: "pratibha@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Mashak Shaikh",
+		// 	email: "mashak@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Vikas Mishra",
+		// 	email: "vikas@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// {
+		// 	name: "Yudi Krishna",
+		// 	email: "yudi@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+		// { name: "Hanif", email: "hanif@innoventure.ae", role: "agent", role_id: 5 },
+		// {
+		// 	name: "Deepti",
+		// 	email: "deepti@innoventure.ae",
+		// 	role: "agent",
+		// 	role_id: 5,
+		// },
+
 		{
-			name: "Divya Lekshmi",
-			email: "divya@innoventure.ae",
+			name: "Ravi",
+			email: "ravi@innoventure.ae",
 			role: "agent",
 			role_id: 5,
 		},
-		{ name: "Bipin", email: "bipin@innoventure.ae", role: "agent", role_id: 5 },
 		{
-			name: "Mishiel",
-			email: "mishiel@innoventure.ae",
+			name: "Ahmad Saber",
+			email: "ahmed@innoventure.ae",
 			role: "agent",
 			role_id: 5,
 		},
-		{ name: "Kent", email: "kent@innoventure.ae", role: "agent", role_id: 5 },
-		{
-			name: "Mahmoud Gaber",
-			email: "mahmoud@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Sayed Atef",
-			email: "sayed@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{ name: "Nada", email: "nada@innoventure.ae", role: "agent", role_id: 5 },
-		{ name: "Sinan", email: "sinan@innoventure.ae", role: "agent", role_id: 5 },
-		{
-			name: "Abdallah Salag",
-			email: "salag@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Mustafa",
-			email: "mustafa@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Abdallah Bakhit",
-			email: "bakhit@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Amil",
-			email: "amil@innoventure.ae",
-			role: "team_lead",
-			role_id: 4,
-		},
-		{
-			name: "Sameer",
-			email: "sameer@innoventure.ae",
-			role: "team_lead",
-			role_id: 4,
-		},
-		{
-			name: "Libi",
-			email: "libi@innoventure.ae",
-			role: "team_lead",
-			role_id: 4,
-		},
-		{
-			name: "Chrisvel J",
-			email: "chrisvel@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{ name: "Imran", email: "imran@innoventure.ae", role: "agent", role_id: 5 },
-		{
-			name: "Prabha SH",
-			email: "prabha@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Talha Ansari",
-			email: "talha@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{ name: "Harsh", email: "harsh@innoventure.ae", role: "agent", role_id: 5 },
-		{
-			name: "Ashwini",
-			email: "ashwini@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Pratibha K",
-			email: "pratibha@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Mashak Shaikh",
-			email: "mashak@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Vikas Mishra",
-			email: "vikas@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{
-			name: "Yudi Krishna",
-			email: "yudi@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
-		{ name: "Hanif", email: "hanif@innoventure.ae", role: "agent", role_id: 5 },
-		{
-			name: "Deepti",
-			email: "deepti@innoventure.ae",
-			role: "agent",
-			role_id: 5,
-		},
+
+
+		
+
+
+
 	];
 
 	const insertedUsers = await User.bulkCreate(users, { returning: true });
